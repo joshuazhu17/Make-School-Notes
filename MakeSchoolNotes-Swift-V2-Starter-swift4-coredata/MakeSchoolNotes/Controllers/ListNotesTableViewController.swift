94e78721a5674670f40a16d4a8d5996d564c216c
joshuazhu17 <joshua.zhu@gmail.com>
2018-07-05 13:37:14 -0700

    Add completed project

---
+++ MakeSchoolNotes-Swift-V2-Starter-swift4-coredata/MakeSchoolNotes/Controllers/ListNotesTableViewController.swift
@@ -0,0 +1,84 @@
+//
+//  ListNotesTableViewController.swift
+//  MakeSchoolNotes
+//
+//  Created by Chris Orcutt on 1/10/16.
+//  Copyright © 2016 MakeSchool. All rights reserved.
+//
+
+import UIKit
+
+class ListNotesTableViewController: UITableViewController {
+    
+    var notes = [Note]() {
+        didSet {
+            tableView.reloadData()
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        notes = CoreDataHelper.retrieveNotes()
+    }
+    
+    @IBAction func unwindWithSegue(_ segue: UIStoryboardSegue) {
+        notes = CoreDataHelper.retrieveNotes()
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // 1
+        return notes.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // 2
+        let cell = tableView.dequeueReusableCell(withIdentifier: "listNotesTableViewCell", for: indexPath) as! ListNotesTableViewCell
+        
+        let note = notes[indexPath.row]
+        cell.noteTitleLabel.text = note.title
+        cell.noteModificationTimeLabel.text = note.modificationTime?.convertToString() ?? "unknown"
+        
+        var displayContent: String
+        guard let noteContent = note.content else {
+            cell.notePreview.text = ""
+            return cell
+        }
+        if noteContent.count > 5 {
+            displayContent = String(noteContent.prefix(5)) + "..."
+        }
+        else {
+            displayContent = noteContent
+        }
+        cell.notePreview.text = displayContent
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            let noteToDelete = notes[indexPath.row]
+            CoreDataHelper.delete(note: noteToDelete)
+            
+            notes = CoreDataHelper.retrieveNotes()
+        }
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let identifier = segue.identifier else { return }
+        
+        switch identifier {
+        case "displayNote":
+            guard let indexPath = tableView.indexPathForSelectedRow else { return }
+            let note = notes[indexPath.row]
+            let destination = segue.destination as! DisplayNoteViewController
+            destination.note = note
+            
+        case "addNote":
+            print("create note bar button item tapped")
+            
+        default:
+            print("unexpected segue identifier")
+        }
+    }
+}
